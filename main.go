0bfde6246a0580072a90abe4929a28df899dd1d2
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-10-31 10:12:49 +0000

    chore: add simple server

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	serverPort = "3430"
+)
 
 func main() {
 	fmt.Println("setup  done!!")
+
+	// catch all routes
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL)
+
+		w.Write([]byte("Hellooo"))
+	})
+
+	fmt.Printf("server is running on port %s\n", serverPort)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", serverPort), nil)
+	if err != nil {
+		log.Fatalln("server error: ", err)
+	}
 }
